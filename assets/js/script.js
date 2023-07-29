7ef04bd192735088a6be670289def5f59eabdf83
BrrunaRibeiro <>
2023-07-29 15:04:52 +0000

    Add basic Js function structure

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+function runGame() {
+
+}
+
+function runTutorial () {
+
+}
+
+function timer() {
+
+}
+
+function displayQandA() {
+
+}
+
+function selectAnswer () {
+
+}
+
+function submitAnswer() {
+
+}
+
+function checkAnswer () {
+
+}
+
+function updateScore () {
+
+}
+
+function back() {
+
+}
+
+function exit() {
+
+}
+
+
