f071d4ffdc94aa0b2f182c746ef9b012296169bd
BrrunaRibeiro <>
2023-07-29 15:42:01 +0000

    Add first event listener test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+console.log("Hello!!");
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "tutorial") {
+                alert("You clicked tutorial!");
+            } else let gameStart = this.getAttribute("data-type");
+            alert(`You clicked ${gameStart}`);
+        }
+        
+        )
+    }
+}
+)
+
+
 function runGame() {
 
 }
