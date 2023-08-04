862621e4ca89d7e90dee9bdfed9f0ae2211d56c4
BrrunaRibeiro <>
2023-08-04 08:26:57 +0000

    Test startgame function and add favicon source to readme file

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 *WireFrames
 Made with "MarvelAp"
 <iframe src="https://marvelapp.com/prototype/bj49j2h?emb=1&iosapp=false&frameless=false" width="1440" height="1024" allowTransparency="true" frameborder="0" style="-webkit-clip-path: inset(2px 2px); clip-path: inset(2px 2px);"></iframe>
+
+*Favicon
+Made with "FavIcon Generator"
+https://favicon.io/
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function () {
+    var div = document.getElementsByTagName("div");
 })
-
 //Assign const var to main elements(Timer, Tutorial button and Start button)
 const countdownTimer = document.getElementById("countdown-timer");
 const tutorialButton = document.getElementById("tutorial");
 const startButton = document.getElementById("start");
-const div = getElementsByTagName("div");
-
-//Add object with all questions and answers
-const question = [
-      {
-        question: "Which country is famous for the ancient pyramids?",
-        options: {
-          optionA: "Greece",
-          optionB: "Egypt",
-          optionC: "Mexico"
-        },
-        correctAnswer: "Egypt"
-      },
-      {
-        question: "In which city is the iconic Eiffel Tower located?",
-        options: {
-          optionA: "Barcelona",
-          optionB: "Rome",
-          optionC: "Paris"
-        },
-        correctAnswer: "Paris"
-      }
-    ]
 
 //Add event listeners
 startButton.addEventListener("click", startGame);
 tutorialButton.addEventListener("click", startTutorial);
 
-//User's inputs
-var userName = document.getElementById("name").value;
-var userAge = document.getElementById("age").value;
+//Add object with all questions and answers
+const question = [
+    {
+        question: "Which country is famous for the ancient pyramids?",
+        options: {
+            optionA: "Greece",
+            optionB: "Egypt",
+            optionC: "Mexico"
+        },
+        correctAnswer: "Egypt"
+    },
+    {
+        question: "In which city is the iconic Eiffel Tower located?",
+        options: {
+            optionA: "Barcelona",
+            optionB: "Rome",
+            optionC: "Paris"
+        },
+        correctAnswer: "Paris"
+    },
+
+]
+
+
+
+//User's inputs(it will be the name will be shown in the Final Score screen)
+function storeInputs() {
+    let userName = document.getElementById("name").value;
+    console.log(userName);
+
+    let userAge = document.getElementById("age").value;
+    console.log(userAge);
+}
 
 
 //Start Tutorial Function
-function startTutorial () {
- //Add image/photo with instructions
- //Add Start button to start the quiz from the tutorial as well
+function startTutorial() {
+    //Add image/photo with instructions
+    //Add Start button to start the quiz from the tutorial as well
 }
 
 //Display question and asnwer options Funtion
 function showQuestion() {
     document.getElementById("question").innerText = question[0].question;
-    document.getElementById("optionA").innerText = questionData.options.optionA;
+    document.getElementById("optionA").innerText = question[0].options.optionA;
     console.log(question[0].question);
 }
 
@@ -70,10 +76,10 @@ function timer() {
 // Check if awnwer selected is the correct answer Function
 function checkAnswer() {
     if (options === correctAnswer) {
-            answeredCorrectly = True;
+        answeredCorrectly = True;
     } else {
         answeredCorrectly = False;
-    }    
+    }
 }
 
 //Submit selected answer Funtion
@@ -92,7 +98,7 @@ function updateScore() {
 
 //See previous question Funtion
 function back() {
-    
+
 }
 
 //Exit game completely and go back to index.html page function
@@ -103,11 +109,16 @@ function exit() {
 
 //Start game
 function startGame() {
-    if (userName || userAge) {
-        div.classList.remove("hide");
-        showQuestion();        
-        } else (!userName || !userAge); {
+    var userName = document.getElementById("name").value;
+    var userAge = document.getElementById("age").value;
+    if (!userName && !userAge) {
         alert("Please enter your Name and Age.");
         return;
-        }
+    } else if (userName && userAge) {
+        console.log("Name:", userName);
+        console.log("Age:", userAge);
+    }
+    //qanda.classList.remove("hide");
+    //input.classList.add("hide");
 }
+
